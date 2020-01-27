0d19826b0c4ea880bef7fe58ed152f81e2339833
deannabosschert <deanna.bosschert@gmail.com>
2020-01-27 14:45:08 +0100

    refactor(setup): change names

--- src/components/testComponent.vue
+++ src/components/testComponent.vue
@@ -1,35 +1,18 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      yeet
-    </p>
+  <div class="test1">
+    <h1>Content</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'testComponent',
+  name: "test1",
   props: {
     msg: String
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+
 </style>
