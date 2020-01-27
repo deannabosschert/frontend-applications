338b50a79d1e36413b87c087105548da2aa13584
deannabosschert <deanna.bosschert@gmail.com>
2020-01-27 14:44:33 +0100

    feat(setup): add switch-toggle

---
+++ src/components/switchBetween.vue
@@ -0,0 +1,71 @@
+<template>
+  <form class="switchBetween" @change="onSwitch">
+    <input
+      type="radio"
+      id="teDoen-input"
+      class="switchBetween_radio"
+      name="switchBetween-input"
+      value="teDoen"
+      checked
+    />
+    <label for="teDoen-input" class="switchBetween_label">
+      Toggle 1
+    </label>
+    <input
+      type="radio"
+      id="incl-alGehaald-input"
+      class="switchBetween_radio"
+      name="switchBetween-input"
+      value="incl-alGehaald"
+    />
+    <label for="incl-alGehaald-input" class="switchBetween_label">
+      Toggle 2
+    </label>
+  </form>
+</template>
+
+<script>
+export default {
+  methods: {
+    onSwitch(event) {
+      if (event.target.value === "teDoen") {
+        return this.$emit("teDoen-click")
+      }
+
+      return this.$emit("incl-alGehaald-click")
+    }
+  }
+}
+</script>
+
+<style>
+.switchBetween {
+  display: flex;
+  padding: 2px;
+  border-radius: 10px;
+  background: white;
+  max-width: 50vw;
+  /* margin-left: 25vw;
+  margin-right: 25vw; */
+}
+
+.switchBetween_label {
+  flex: 0 0 50%;
+  padding: 20px 0;
+  border-radius: 7px;
+  background: white;
+  text-align: center;
+  align-self: center;
+  /* max-width: 12.5vw; */
+  font-weight: 600;
+}
+
+.switchBetween_radio:checked + .switchBetween_label {
+  background: #4b0082;
+  color: white;
+}
+
+input[type="radio"] {
+  visibility: hidden;
+}
+</style>
--- src/index.vue
+++ src/index.vue
@@ -1,25 +1,52 @@
 <template>
   <div id="app">
-    <testComponent msg="and they were roommates"/>
+    <switchBetween
+      @teDoen-click="choice = 'teDoen'"
+      @incl-alGehaald-click="choice = 'incl-alGehaald'"
+    />
+
+    <div v-if="choice === 'teDoen'">
+      <h3>Toggle1header</h3>
+      <testComponent/>
+    </div>
+
+    <div v-if="choice === 'incl-alGehaald'">
+      <h3>Toggle2header</h3>
+      <testComponent/>
+    </div>
   </div>
 </template>
 
 <script>
-import testComponent from './components/testComponent.vue'
+import switchBetween from "./components/switchBetween.vue"
+import testComponent from "./components/testComponent.vue"
+
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      choice: "teDoen"
+    }
+  },
   components: {
+    switchBetween,
     testComponent
   }
 }
 </script>
 
 <style>
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  align-items: center;
+  align-content: center;
 }
 </style>
